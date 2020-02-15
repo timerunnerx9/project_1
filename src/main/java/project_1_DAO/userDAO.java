4b48081f4f0ed4dcd62e879aaede04aa4a8c600c
davecen9 <davecen9@gmail.com>
2020-02-14 21:43:48 -0500

    added verifed users

--- src/main/java/project_1_DAO/userDAO.java
+++ src/main/java/project_1_DAO/userDAO.java
@@ -1,5 +1,36 @@
 package project_1_DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import project_1.utils.ConnectionUtil;
+
 public class userDAO {
 
+	
+	
+	
+	public static Boolean varifyuser(String userid) {
+		try(Connection connection = ConnectionUtil.getConnection()){
+			String sql = "SELECT * FROM ers_users WHERE userid = ?";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setString(1, userid);
+			
+			ResultSet result = statement.executeQuery();
+			
+			if(result.next()) {
+				return true;
+			}
+			else {
+				return false;
+			}
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+}
 }
--- src/main/java/project_1_systemFunctions/loginSession.java
+++ src/main/java/project_1_systemFunctions/loginSession.java
@@ -1,5 +1,13 @@
 package project_1_systemFunctions;
 
 public class loginSession {
-
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	
+	
 }
+
