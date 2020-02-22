fb091f6ade0df510f2ed64ca46e75fadf60ed88b
davecen9 <davecen9@gmail.com>
2020-02-22 14:38:22 -0500

    2:38

--- src/main/java/project_1/utils/JsonUtil.java
+++ src/main/java/project_1/utils/JsonUtil.java
@@ -3,19 +3,25 @@ package project_1.utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import project_1_DAO.userDAO;
+
 public class JsonUtil {
-	private static ObjectMapper om;
-	{
-		om = new ObjectMapper();
-	}
+//	public static void main(String[] args) {
+//		System.out.println(javaToJson(userDAO.getUser(1)));
+//	}
+//	
 	public static String javaToJson(Object obj) {
+		ObjectMapper om = new ObjectMapper();
+		
 		String jsonString ="";
 		
 		try {
-			jsonString = om.writeValueAsString(obj);
-		} catch (JsonProcessingException e) {
+			 jsonString = om.writeValueAsString(obj);
+			 
+		} catch (Exception e) {
 			
-			System.out.println("Exception occured while converting java object to json");
+			e.printStackTrace();
+//			System.out.println("Exception occured while converting java object to json");
 		}
 		return jsonString;
 	}
--- src/main/java/project_1_servlets/UserServlet.java
+++ src/main/java/project_1_servlets/UserServlet.java
@@ -1,36 +1,47 @@
 package project_1_servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import project_1_service.UserService;
+
 import project_1_DAO.userDAO;
+import project_1_baseClasses.User;
+import project_1_service.UserService;
 
 /**
  * Servlet implementation class UserServlet
  */
 @WebServlet("/UserServlet")
 public class UserServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private UserService userService;
+//	public static void main(String[] args) {
+//		
+//		UserService userService = new UserService();
+//		String jsonUser = userService.getUserJson(userDAO.getUser(1));
+//		System.out.println(jsonUser);
+//	}
+	
+//	private static final long serialVersionUID = 1L;
 	
 	
 	
-	@Override
-	public void init() throws ServletException {
-		userService = new UserService();
-		try {
-			Class.forName("org.postgresql.Driver");
-		}
-		catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		super.init();
-	}
-       
+//	@Override
+//	public void init() throws ServletException {
+//		
+//		try {
+//			Class.forName("org.postgresql.Driver");
+//			
+//		}
+//		catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		super.init();
+//	}	
+	
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -39,23 +50,19 @@ public class UserServlet extends HttpServlet {
 		resp.addHeader("Access-Control-Allow-Methods", "GET POST PUT DELETE");
 		resp.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
 		
-		
 		super.service(req, resp);
+		
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		UserService userService = new UserService();
 		String jsonUser = userService.getUserJson(userDAO.getUser(1));
-		response.setContentType("application/json");
-		response.getWriter().write(jsonUser);
+//		response.setContentType("application/json");
+		response.getWriter().append(jsonUser);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		doGet(request, response);
