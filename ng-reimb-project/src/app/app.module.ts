13d20f9703c87ea4a0dc125ec8e80031f5b2e6a8
davecen9 <davecen9@gmail.com>
2020-02-21 20:51:40 -0500

    created angular project

---
+++ ng-reimb-project/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContentComponent } from './components/content/content.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
