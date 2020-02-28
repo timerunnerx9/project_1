7340acffeb2d02fdd7d8e2f1eb1114f002dbfe0f
amikchowdhury <amikc1996@gmail.com>
2020-02-28 09:03:39 -0500

    added passhash

--- src/main/java/project_1/utils/PassHash.java
+++ src/main/java/project_1/utils/PassHash.java
@@ -1,4 +1,9 @@
 package project_1.utils;
+//1. We need to make our passwords very long so that Mitch and Josh doesn't shit on us for it.
+//At least 12 characters long.
+
+//2. We have to generate salt for the password using a CSPRNG. In java we use, java.security.SecureRandom.
+
 
 public class PassHash {
 
