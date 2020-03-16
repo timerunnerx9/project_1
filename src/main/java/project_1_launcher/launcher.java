9a4a881fbac877ffffd946ef59ec599d340b2757
davecen9 <davecen9@gmail.com>
2020-03-16 08:20:01 -0400

    fixed bugs

--- src/main/java/project_1_launcher/launcher.java
+++ src/main/java/project_1_launcher/launcher.java
@@ -4,6 +4,13 @@ import project_1_DAO.UserDAO;
 
 public class launcher {
 	public static void main(String[] args) {
+		
+		/* if there's issue logging in
+		1.change the password in database to 294597053
+		2.run UserDAO.updatePassByID(1)
+		3.print it out to test if it's true
+		
+		*/
 		UserDAO.updatePassByID(1);
 		
 		System.out.println(PassHash.checkPass("294597053",UserDAO.getUserByUsername("davecen9").getPassword()));
